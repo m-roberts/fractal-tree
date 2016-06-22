d1e3386b33f558350f18f401b8d504a218cf9411
Faraz Khan <farazcsk@gmail.com>
2016-06-22 18:41:27 +0100

    Fixed issue where SW would not continue to work after first reload

--- sw.js
+++ sw.js
@@ -21,10 +21,16 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
 });
